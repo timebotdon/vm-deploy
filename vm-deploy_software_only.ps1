805f140ff46c4bb5b1edee3aa739c2c701280c76
Donovan Choy <46291949+timebotdon@users.noreply.github.com>
2019-06-25 17:57:48 +0800

    add software-only installer

--- vm-deploy_software_only.ps1
+++ vm-deploy_software_only.ps1
@@ -8,59 +8,6 @@ function elevateUAC {
 	}
 }
 
-
-## Network Config
-
-Function setNetworking {
-	$networkCh = read-host -Prompt "Use Static/DHCP? (s/d)"
-	if ($networkCh -eq "s") {
-		Get-WmiObject win32_networkadapter -filter "netconnectionstatus = 2" | select Name
-		$interface = read-host -Prompt "Interface Name"
-		$ip = read-host -Prompt "IP Address"
-		$netmask = read-host -Prompt "Netmask"
-		$gateway = read-host -Prompt "Gateway Address"
-		$netConfirmS = read-host -Prompt "Confirm? (y/n)"
-		if ($netConfirmS -eq "y") {
-			netsh interface ip set address name="$interface" static $ip $netmask $gateway
-			Write-host "Testing network connection.."
-			ping $gateway
-			if ($lastexitcode -eq "0") {
-				Write-host "Ping test successful."
-			}
-			else {
-				Write-host "Connection Error. Please check network settings / firewall rules!"
-				menu
-			}
-		}
-	}
-	if ($networkCh -eq "d") {
-		Get-WmiObject win32_networkadapter -filter "netconnectionstatus = 2" | select Name
-		$interface = read-host -Prompt "Interface Name"
-		$netConfirmD = read-host -Prompt "Confirm? (y/n)"
-		if ($netConfirmD -eq "y") {
-			netsh interface ip set address "$interface" dhcp
-			Write-host "Testing network connection.."
-			ping $gateway
-			if ($lastexitcode -eq "0") {
-				Write-host "Ping test successful."
-			}
-			else {
-				Write-host "Connection Error. Please check network settings / firewall rules!"
-				menu
-			}
-		}
-	}
-	menu
-}
-
-## Domain
-
-Function joinDomain {
-	$domainName = read-host -Prompt "Domain Name?"
-	Add-Computer –DomainName $domainName –Credential (Get-Credential)
-	menu
-}
-
 ## Package Management
 Function checkChoco {
 	Write-Host "Checking if chocolatey is installed."
@@ -118,21 +65,14 @@ Function installSoftware {
 function menu {
 	Write-host 
 	Write-host "Script for quick deployment of new virtual machines. Installs software using Chocolatey package manager."
-	Write-host "1. Configure networking"
-	Write-host "2. Install software"
-	Write-host "3. Join Domain"
+	Write-host "1. Install software"
 	Write-host "0. Exit"
 	Write-host
 	$menuCh = read-host -Prompt "Choose an option."
 	
+
 	if ($menuCh -eq "1") {
-		setNetworking
-	}
-	if ($menuCh -eq "2") {
 		installSoftware
-	}
-	if ($menuCh -eq "3") {
-		joinDomain
 	}
 	if ($menuCh -eq "0") {
 		exit
